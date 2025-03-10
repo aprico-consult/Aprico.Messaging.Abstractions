c4663bb144304756e003469d6009e7a993e5f8ae
François Chabot <francois.chabot@stateless.be>
2025-03-10 18:48:56 +0100

    Change return type of DeserializeBody from dynamic to object
    Updated the return type of DeserializeBody to use object instead of dynamic for better type safety and clarity. Removed remarks related to double dispatch and adjusted the method documentation accordingly.

--- src/Aprico.Messaging.Abstractions/Messaging/Abstractions/IMessageDisassembler.cs
+++ src/Aprico.Messaging.Abstractions/Messaging/Abstractions/IMessageDisassembler.cs
@@ -40,12 +40,6 @@ public interface IMessageDisassembler<in TMessage>
 	/// The <typeparamref name="TMessage"/> message to be disassembled and whose payload requires
 	/// deserialization.
 	/// </param>
-	/// <returns>The deserialized payload of the message as a <see langword="dynamic"/>.</returns>
-	/// <remarks>
-	/// Returning a <see langword="dynamic"/>, instead of a simple <see cref="object"/>, enables double dispatch scenarios by
-	/// facilitating the invocation of the most appropriate method for handling the specific runtime type of the body without requiring
-	/// a visitor pattern.
-	/// </remarks>
-	/// <seealso href="https://en.wikipedia.org/wiki/Double_dispatch#Double_dispatch_in_C#">Double dispatch in C#</seealso>
-	dynamic DeserializeBody(TMessage message);
+	/// <returns>The deserialized message payload object.</returns>
+	object DeserializeBody(TMessage message);
 }
